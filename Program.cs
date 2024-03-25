9ca0097cb1308e891db44a94ecd8260cf935af84
AliHMohammad <alihmohammad@hotmail.com>
2024-03-25 21:22:40 +0100

    removed swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using PokemonApp;
 using PokemonApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +8,8 @@ builder.Services.AddControllers();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 
 // Add database connection
